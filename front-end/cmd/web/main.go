b84e904ae9340d55054d75f50071834817410cfa
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-04-16 09:58:55 +0100

    Add files via upload

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -8,10 +9,12 @@ import (
 )
 
 func main() {
+	// the handler to display our page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
+	// start the web server
 	fmt.Println("Starting front end service on port 80")
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
@@ -19,27 +22,42 @@ func main() {
 	}
 }
 
-func render(w http.ResponseWriter, t string) {
+//go:embed templates
+var templateFS embed.FS
 
+// render generates a page of html from our template files
+func render(w http.ResponseWriter, t string) {
+	// all the required templates for any page
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
+	// append the template we received as a parameter
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	// parse the templates
+	//tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	var data struct{
+		BrokerURL string 
+	}
+
+	data.BrokerURL ="http://localhost:8080"
+
+
+	// execute the template
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
